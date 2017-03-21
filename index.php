88de843b602209436054502ab77916d4338707c6
ekeemmanuel <ekeemmanuel@gmail.com>
2017-03-21 22:48:22 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eke
+ * Date: 21/03/2017
+ * Time: 22:46
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
