576eab75f1b671dd550ab0725d648f4177cbb165
ekeemmanuel <ekeemmanuel@gmail.com>
2017-03-26 21:45:41 +0100

    Sunday

--- connectionScript.php
+++ connectionScript.php
@@ -30,5 +30,5 @@ if (!$link) {
 define('DB_USERNAME', 'your-username');
 define('DB_PASSWORD', 'your-password');
 define('DB_DATABASE', 'your-database');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
