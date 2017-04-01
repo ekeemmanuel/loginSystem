2e144cb6a680da8a97d21f6e98dfbf4b24865677
ekeemmanuel <ekeemmanuel@gmail.com>
2017-04-01 16:59:38 +0100

    Sat'day2

--- .idea/Mike.php
+++ .idea/Mike.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html> <html lang="en">
-    <head> <meta charset="UTF-8">
+    <head>
+        <meta charset="UTF-8">
         <title>Michael Crabb - CV</title>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
     </head>
     <body>
         <header> <h1>Michael Crabb - CV</h1> </header>
         <main>
             <section> <h2>Work History</h2> </section>
             <section> <h2>Education</h2> </section>
+            <br><br><br>Party's over, time to go  <a href="index.php">home.</a><br>
         </main>
     <footer> <p>Last Updated 19/9/2016</p> </footer>
 </body>
